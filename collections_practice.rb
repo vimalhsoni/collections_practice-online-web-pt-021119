92982b02d0512eb53d84f5fb4273cd3f53ca3897
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-22 00:04:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort{|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[0],array[1],array[2] = array[0], array[2],array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |person|
+    person[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject {|x,element| x + element }
+end
+
+#def add_s(array)
+#  array.drop(1).each {|word| word.insert(-1,'s')}
+#end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+    word
+  else
+  word.insert(-1,'s')
+end
+end
+end
